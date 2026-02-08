56df5a79e6edbb88d3ae54851e87ded2dde86a48
Anthony Nazarov <anmstudios21c@gmail.com>
2026-02-08 17:08:10 -0500

    Enable all home page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
+import HomeAbout from "@/components/home-about";
+import HomeBlog from "@/components/home-blog";
+import HomeCharts from "@/components/home-charts";
+import HomeCta from "@/components/home-cta";
 import HomeDashboard from "@/components/home-dashboard";
+import HomeFeed from "@/components/home-feed";
+import HomeHero from "@/components/home-hero";
+import HomeStakeholders from "@/components/home-stakeholders";
 
 export default function Home() {
   return (
     <div className="landing">
-      {/* <HomeHero />
-      <HomeAbout /> */}
+      <HomeHero />
+      <HomeAbout />
       <HomeDashboard />
-      {/* <HomeBlog />
+      <HomeBlog />
       <HomeStakeholders />
       <HomeCharts />
       <HomeFeed />
-      <HomeCta /> */}
+      <HomeCta />
     </div>
   );
 }
